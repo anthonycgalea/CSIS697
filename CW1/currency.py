8d9907a3e7c38616b104e63705d02e6779ac02e2
anthonycgalea <44701741+anthonycgalea@users.noreply.github.com>
2021-01-16 12:34:06 -0500

    comments

--- CW1/currency.py
+++ CW1/currency.py
@@ -1,11 +1,11 @@
 while(True):
-    k = input("Enter the currency in knuts (enter Q to quit):\t")
-    if (k == 'Q'):
+    k = input("Enter the currency in knuts (enter Q to quit):\t") #get input
+    if (k == 'Q'): #for quitting
         break
     else:
-        k = int(k)
-        g = k//493
-        k2 = k-g*493
-        s = k2//29
-        k2 = k2 - s*29
+        k = int(k) #convert input to int
+        g = k//493 #get galleons
+        k2 = k-g*493 #remove galleons from knut count
+        s = k2//29 #get sickles
+        k2 = k2 - s*29 #remove sickles from knut count
         print(s,"knuts =",g,"galleons",s,"sickles and",k2,"knuts")
